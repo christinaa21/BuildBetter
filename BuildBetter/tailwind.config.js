b76b97f391b883833f98b6a2f78d988e08896f8d
christinaa21 <16521224@mahasiswa.itb.ac.id>
2025-02-03 18:10:29 +0700

    SETUP: Setup Color Theme

--- BuildBetter/tailwind.config.js
+++ BuildBetter/tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customWhite: {
+          '50': '#FFFFFF',
+          '100': '#F5F5F9'
+        },
+        customGray: {
+          '50': '#D9D9D9',
+          '100': '#C6C6C6'
+        },
+        customGreen: {
+          '50': '#BBCBCD',
+          '100': '#ABC4BE',
+          '200': '#45605A',
+          '300': '#30534B', // Primary
+          '400': '#36534D',
+          '500': '#15322C'
+        },
+        customOlive: {
+          '50': '#3F473D', // Primary
+          '100': '#222920'
+        }
+      }
+    },
   },
   plugins: [],
 }
